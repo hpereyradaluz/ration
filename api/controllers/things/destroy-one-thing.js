bf50abeb18a616199408904690cd7e97cead5414
hernan <hpereyradaluz@gmail.com>
2019-03-05 09:54:55 -0300

    contact support if thing not exists

--- api/controllers/things/destroy-one-thing.js
+++ api/controllers/things/destroy-one-thing.js
@@ -63,6 +63,11 @@ module.exports = {
 
   exits: {
 
+    notFound:{
+      description:'No such thing with that ID exits.',
+      responseType: 'notFound'//res.notFound() in the sails docs if you see them
+    },
+
     forbidden:{
       description: 'The user making this request doesn\'t have the permissions to delete this thing.',
       responseType: 'forbidden'
@@ -75,6 +80,10 @@ module.exports = {
     var thing = await Thing.findOne({
       id: inputs.id
     });
+
+    if(!thing){
+      throw 'notFound';
+    }
 
     if (thing.owner !== this.req.me.id){
       throw 'forbidden';
