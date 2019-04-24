59a9e3ed477d03f8a7bbd5afac144a7ef5f7d542
hernan <hpereyradaluz@gmail.com>
2019-04-24 14:29:42 -0300

    deploy

--- config/env/production.js
+++ config/env/production.js
@@ -149,7 +149,7 @@ module.exports = {
     ***************************************************************************/
     cors: {
       // allowOrigins: [
-      //   'https://rationstaging.herokuapp.com',
+        'https://rationstaging.herokuapp.com',
       // ]
     },
 
